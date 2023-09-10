d5004c5e685d141e10f4ad1d67a79dd0dce9d814
SaraRamadan25 <sarahramadanelsayed@gmail.com>
2023-09-10 16:20:17 +0300

    WIP

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -6,30 +6,29 @@ use App\Enums\PetType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pet extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'type',
-        'avatar',
-        'date_of_birth',
-        'owner_id',
-    ];
     protected $casts = [
         'type' => PetType::class,
     ];
 
     public function owner(): BelongsTo
     {
-        return $this->belongsTo(Owner::class);
+        return $this->belongsTo(User::class, 'owner_id');
     }
 
     public function appointments(): HasMany
     {
         return $this->hasMany(Appointment::class);
+    }
+
+    public function clinic(): BelongsToMany
+    {
+        return $this->belongsToMany(Clinic::class);
     }
 }
---
+++ app/Models/Slot.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Slot extends Model
+{
+    use HasFactory;
+
+    public function appointments(): HasMany
+    {
+        return $this->hasMany(Appointment::class);
+    }
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
